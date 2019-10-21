585a76f3a42d96f7b3831b1a53e97f58bf432fd6
René Schwarzinger <web@milleniumfrog.de>
2019-10-21 14:44:19 +0200

    chore: configure storybook
    add addons and configure everything

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
 import { configure } from '@storybook/vue';
 
+// set viewports
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
+    defaultViewport: 'responsive',
+  },
+});
+
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /\.stories\.tsx?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  });
+
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+      },
+      // Optional
+    ],
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
+};
